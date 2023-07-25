7dfdedd51a7de88d8bf30431bc8210b5c0048a21
taichanne30 <dev@taichan.site>
2023-07-26 00:47:44 +0900

    fix: 数値が0の時にデフォルト値が採用されてしまう問題

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -5,9 +5,9 @@ const envFile = process.env.NODE_ENV === 'development' ? '.env.development' : '.
 require('dotenv').config({ path: `./${envFile}` })
 
 const today = new Date()
-const recordTimeHour = Number(process.env?.RECORD_HOUR) || 3
-const recordTimeSEC = Number(process.env?.RECORD_MINUTE) || 34
-export const recordTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), recordTimeHour, recordTimeSEC, 0, 0)
+const recordTimeHour = process.env.RECORD_HOUR !== undefined ? Number(process.env.RECORD_HOUR) : 3
+const recordTimeMinute = process.env.RECORD_MINUTE !== undefined ? Number(process.env.RECORD_MINUTE) : 34
+export const recordTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), recordTimeHour, recordTimeMinute, 0, 0)
 
 export const postTitle = process.env?.POST_TITLE || `Today's 334 Top 10`
 export const remindPostText = process.env?.REMINED_POST_TEXT || `334観測中`
