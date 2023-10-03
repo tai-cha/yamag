a861cf385637d33cd4ea2c5c1d5c208ceaf7d111
taichanne30 <dev@taichan.site>
2023-10-04 01:55:36 +0900

    前の日の結果を取得可能にする

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -9,7 +9,14 @@ const parseIntWithDefalult = (v:any, def:number):number => Number.isFinite(parse
 const today = new Date()
 const recordTimeHour = parseIntWithDefalult(process.env.RECORD_HOUR, 3)
 const recordTimeMinute = parseIntWithDefalult(process.env.RECORD_MINUTE, 34)
-export const recordTime = new Date(today.getFullYear(), today.getMonth(), today.getDate(), recordTimeHour, recordTimeMinute, 0, 0)
+export const recordTime = (() => {
+  // 23:59など集計が日付を挟む場合には1日前を参照する
+  let target = new Date(today.getFullYear(), today.getMonth(), today.getDate(), recordTimeHour, recordTimeMinute, 0, 0)
+  const todayMidnight = new Date(today.getFullYear(), today.getMonth(), today.getDate(), 0, 0, 0, 0)
+  // 一日前 = 86_400_000秒前
+  if (target.getTime() - todayMidnight.getTime()) target = new Date(target.getTime() - 86_400_000)
+  return target
+})()
 
 export const postTitle = process.env?.POST_TITLE || `Today's 334 Top 10`
 export const remindPostText = process.env?.REMINED_POST_TEXT || `334観測中`
@@ -29,6 +36,10 @@ export const server:Server = {
   credential: process.env.SERVER_TOKEN || ''
 }
 
+export const Experimental = {
+  useUntil: process.env?.EXPERIMENTAL_USE_UNTIL === 'TRUE'
+}
+
 export const isDbEnabled = ():boolean => !!DATABASE_URL
 
-export default {recordTime, postTitle, remindPostText, matcher, userName, server, isDbEnabled, mention}
+export default {recordTime, postTitle, remindPostText, matcher, userName, server, isDbEnabled, mention, Experimental}
